2693f298df6a895ffe2d26aabc3e724471a349b4
Suman Humagain <returnzerosh@gmail.com>
2024-12-08 22:15:02 -0500

    plain decorator and uppercase implementation added

---
+++ Decorator_Design_Pattern/PlainDecorator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator_Design_Pattern
+{
+    public class PlainDecorator : Decorator<string>
+    {
+        public PlainDecorator(IComponent<string> component) : base(component) { }
+
+        public override string GetText()
+        {
+            return $"Test- {base.GetText()}"; // added prefix
+        }
+    }
+
+}
---
+++ Decorator_Design_Pattern/UpperCaseDecorator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator_Design_Pattern
+{
+    public class UpperCaseDecorator : Decorator<string>
+    {
+        public UpperCaseDecorator(IComponent<string> component) : base(component) { }
+
+        public override string GetText()
+        {
+            return base.GetText().ToUpper(); // converting text of the base class in upper case
+        }
+    }
+
+}
