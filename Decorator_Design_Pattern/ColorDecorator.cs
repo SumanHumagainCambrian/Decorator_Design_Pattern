380085de0f132458257386e0b05a78b6b792a91f
Suman Humagain <returnzerosh@gmail.com>
2024-12-08 22:17:59 -0500

    Added ColorDecorator

---
+++ Decorator_Design_Pattern/ColorDecorator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator_Design_Pattern
+{
+    public class ColorDecorator : Decorator<string>
+    {
+        public ColorDecorator(IComponent<string> component) : base(component) { }
+
+        public override string GetText()
+        {
+            return $"\u001b[31m{base.GetText()}\u001b[0m"; // Used ANSI escape code to change the color of text
+        }
+    }
+}
