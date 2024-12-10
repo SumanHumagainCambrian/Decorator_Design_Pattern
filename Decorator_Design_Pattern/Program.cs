6efee2ad6ea17e7faab18ecd1f503a7e3151a1ac
Suman Humagain <returnzerosh@gmail.com>
2024-12-10 18:05:44 -0500

    Fixes

--- Decorator_Design_Pattern/Program.cs
+++ Decorator_Design_Pattern/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Decorator_Design_Pattern;
+
+Console.WriteLine("--Decorator design pattern--\n");
+
+// Create the base component
+IComponent<string> component = new ConcreteComponent();
+
+IComponent<string> colorDecorator = new ColorDecorator(component);
+IComponent<string> plainDecorator = new PlainDecorator(component);
+IComponent<string> upperCaseDecorator = new UpperCaseDecorator(component);
+
+//Outputs
+Console.WriteLine("Parent Component: " + component.GetText());
+Console.WriteLine("Plain Decorator: " + plainDecorator.GetText());
+Console.WriteLine("UpperCase Decorator: " + upperCaseDecorator.GetText());
+Console.WriteLine("Color Decorator: " + colorDecorator.GetText());
+
+Console.ReadLine();
